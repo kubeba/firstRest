0988e1a599ccc409f84db68f8fa54c017a46f9d5
Serr Nicolas <qxj4421@europe.bmw.corp>
2016-08-09 17:56:38 +0200

    added hikariconfig

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,7 +2,12 @@ package com.example;
 
 import java.io.IOException;
 
+import javax.sql.DataSource;
+
 import org.glassfish.grizzly.http.server.HttpServer;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 /**
  * Main class.
@@ -33,12 +38,30 @@ public class Main {
      */
     public static void main(String[] args) throws IOException {
     	
+    	String dbFile = "";
     	
-        final HttpServer server = Utils.startServer();
-        System.out.println(String.format("Jersey app started with WADL available at "
-                + "%sapplication.wadl\nHit enter to stop it...", Utils.BASE_URI));
-        System.in.read();
-        server.stop();
+    	HikariConfig hikariConfig = new HikariConfig();
+    	hikariConfig.setAutoCommit(false);
+    	hikariConfig.setConnectionTimeout(5000);
+        hikariConfig.setDriverClassName(org.hsqldb.jdbc.JDBCDriver.class.getName());
+        hikariConfig.setIdleTimeout(120000L);
+        hikariConfig.setInitializationFailFast(true);
+        hikariConfig.setJdbcUrl("jdbc:hsqldb:file:" + dbFile + ";hsqldb.write_delay=false");
+        hikariConfig.setMaximumPoolSize(pConfig.getMaxPoolSize());
+        hikariConfig.setPassword("sa123");
+//        hikariConfig.setPoolName(“test pool");
+        hikariConfig.setUsername("SA");
+        
+    	DataSource ds = new HikariDataSource(hikariConfig);
+    	
+    	
+
+    	
+//        final HttpServer server = Utils.startServer();
+//        System.out.println(String.format("Jersey app started with WADL available at "
+//                + "%sapplication.wadl\nHit enter to stop it...", Utils.BASE_URI));
+//        System.in.read();
+//        server.stop();
     }
 }
 
