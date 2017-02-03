304fa3abe72d5996992bdecfbd90a81d41fb5e1b
GCDM <gcdm@Nicolass-MacBook-Pro.local>
2017-02-03 17:59:29 +0100

    Database moved

--- src/main/java/classes/MyConstants.java
+++ src/main/java/classes/MyConstants.java
@@ -2,7 +2,7 @@ package classes;
 
 public final class MyConstants {
 
-	private static final String dbConnection = "jdbc:hsqldb:file:/Users/Shared/hsqldb/databases/myUserDb;hsqldb.write_delay=false";
+	private static final String dbConnection = "jdbc:hsqldb:file:database/myUserDb;hsqldb.write_delay=false";
 	private static final String dbUser = "SA";
 	private static final String dbPassword = "sa";
 	
